48e5fb8102899adf08559e124478a590d63a21ef
ongaram <ohjiho0106@gmail.com>
2021-05-24 22:39:48 +0900

    commit

--- main.js
+++ main.js
@@ -1,17 +1,10 @@
-$(document).ready(function(){
-    var target = $(".sidebar");
-
-    // 버튼을 클릭하면 사이드바 열림
-    $(document).on("click", ".OpenBtn", function (e){
-        target.show();
-        target.addClass('emphasized');
-    });
-
-    // 사이드바 외부를 클릭하면 사이드바 닫힘
-    $(document).mouseup(function (e){
-        if(target.has(e.target).length==0) {
-            target.hide();
-            target.removeClass('emphasized');
-        } 
-    });
+var swiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 });
